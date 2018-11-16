249db5405d48406c2052062fac1174a99450e88a
vpatrin <victor.patrin150@gmail.com>
2018-11-16 15:06:34 +0100

    dir changes

--- bin/kafka/create-all-topics.sh
+++ bin/kafka/create-all-topics.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd ../kafka_2.11-2.0.0
+cd ../../kafka_2.11-2.0.0
 
 ./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic queue1
 ./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic queue2
--- bin/kafka/start-zookeeper.sh
+++ bin/kafka/start-zookeeper.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cd ../kafka_2.11-2.0.0
+cd ../../kafka_2.11-2.0.0
 ./bin/zookeeper-server-start.sh config/zookeeper.properties
